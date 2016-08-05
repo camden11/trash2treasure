4fc840c72a8ed9b0adce5b504786747d7f9dc1c3
Camden Phalen <camden2112@gmail.com>
2016-08-04 19:48:00 -0700

    db migration for sales

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160727203508) do
+ActiveRecord::Schema.define(version: 20160805024514) do
 
   create_table "organizations", force: true do |t|
     t.string   "name"
@@ -22,6 +22,15 @@ ActiveRecord::Schema.define(version: 20160727203508) do
   end
 
   add_index "organizations", ["name"], name: "index_organizations_on_name", unique: true
+
+  create_table "sales", force: true do |t|
+    t.string   "name"
+    t.integer  "organization_id"
+    t.datetime "start_time"
+    t.datetime "end_time"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", force: true do |t|
     t.string   "name"
