c1ad33c537ec03bc6ce01aefd09e6d8f1e436111
Camden Phalen <camden2112@gmail.com>
2016-08-04 22:31:47 -0700

    working login

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,23 @@
+class SessionsController < ApplicationController
+
+  def new
+  end
+
+  def create
+    user = User.find_by(email: session_params[:email].downcase)
+    if user && user.authenticate(session_params[:password])
+      log_in user
+      redirect_to user.organization
+    else
+      flash.now[:danger] = 'Invalid email/password combination'
+      render 'new'
+    end
+  end
+
+  def destroy
+  end
+
+  def session_params
+    params.require(:session).permit(:email, :password)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
 
+  get 'sessions/new'
+
   get 'signup' => 'users#new'
+
+  get 'login' => 'sessions#new'
+  post 'login' => 'sessions#create'
 
   resources :users, only: [:new, :create]
   resources :organizations, only: [:show]
