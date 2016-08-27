2afcb674d1510a6b4f440a23b0221d61614c1f0f
Camden Phalen <camden2112@gmail.com>
2016-08-27 16:43:54 -0400

    temporarily change image placeholder

--- app/models/organization.rb
+++ app/models/organization.rb
@@ -3,7 +3,7 @@ class Organization < ActiveRecord::Base
   has_many :users
   has_many :sales
 
-  has_attached_file :image, default_url: "image_placeholder.jpg", styles: { small: "150x150#" }
+  has_attached_file :image, default_url: "image_placeholder.png", styles: { small: "150x150#" }
   validates_attachment_content_type :image, :content_type => ["image/jpg", "image/jpeg", "image/png"]
 
   validates :name, presence: true, length: { maximum: 50 }, uniqueness: true
