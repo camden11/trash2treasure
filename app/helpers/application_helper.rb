e2caf2ff76d15195c3fd3665261400c76347a790
Camden Phalen <camden2112@gmail.com>
2016-08-27 18:07:12 -0400

    layout fixes

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,9 @@
 module ApplicationHelper
 
   def body_class_name
-    "#{controller.controller_name} #{controller.action_name}"
+    classes = "#{controller.controller_name} #{controller.action_name}"
+    classes += " adjust-body-position" if @sale_nav && active_shopper?(@sale)
+    classes
   end
 
 end
